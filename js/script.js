652f25280d3bb097fd1ea350351a43ae17b17c65
dacasfilipe <ezequiasmoreira@hotmail.com>
2025-03-18 22:05:49 -0300

    update index.html

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function salvarUsuario(){
+    //cria um objeto usuario
+    let usuario = {
+        name: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("senha").value,
+        phone: document.getElementById("phone").value,
+        birthDate: document.getElementById("birthDate").value,
+        roles: document.getElementById("roles").value
+    }
+    console.log(usuario);
+    //formato json
+    // {
+    //     "name": "John Doe",
+    //     "email": "john@example.com",
+    //     "phone": "123456789",
+    //     "password":"123",
+    //     "birthDate": "1990-01-01",
+    //     "roles": "USER"
+    // }
+    //envia os dados para a API(backend)
+    //substituir o postman(mesmos dados)
+    fetch("http://localhost:8080/api/users",{
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(usuario)
+    })
+    .then(response => response.json())
+    .then(data => console.log("Usuário salvo:", data))
+    .catch(error => console.error("Erro ao salvar usuário:", error));
+}
